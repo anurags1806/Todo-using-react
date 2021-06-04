782d2eba0fd1efee2dc505514111c9299316ed94
unknown <anurags1806@gmail.com>
2021-06-04 19:40:07 +0530

    adding files

---
+++ src/MyComponents/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>
+        <h3>What is a ToDo List?</h3><br/>
+        The definition is a simple one. It's a list of
+        tasks you need to complete, or things that you want to do. Most
+        typically, they're organised in order of priority. Traditionally,
+        they're written on a piece of paper or post it notes and acts as a
+        memory aid.
+      </p>
+    </div>
+  );
+};
